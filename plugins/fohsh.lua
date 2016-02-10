57064124a875c888ae11f628d0d80675c358a532
hafez16 <janboofi.uoovh@gmail.com>
2016-02-10 19:59:18 +0330

    Create fohsh.lua

---
+++ plugins/fohsh.lua
@@ -0,0 +1,18 @@
+
+do
+
+function run(msg, matches)
+  return "کس ننت کیرم تو کونت مادر جنده کس کش گاییدم ننتو".. matches[1]
+end
+
+return {
+  description = "Says hello to someone", 
+  usage = "say hello to [name]",
+  patterns = {
+    "^[!/]fohsh(.*)$",
+    "^[!/]fohsh(.*)$"
+  }, 
+  run = run 
+}
+
+end
