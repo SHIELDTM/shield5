dd7b2d8316c0a7b7a74b276f061fc4630692ffff
hafez16 <janboofi.uoovh@gmail.com>
2016-02-11 11:16:20 +0330

    Update antibadword.lua

--- plugins/antibadword.lua
+++ plugins/antibadword.lua
@@ -1,25 +1,22 @@
-
-
-
 local function run(msg)
-    
+
     local data = load_data(_config.moderation.data)
-    
-     if data[tostring(msg.to.id)]['settings']['lock_badw'] == 'yes' then
-      
-    
+
+     if data[tostring(msg.to.id)]['settings']['lock_link'] == 'yes' then
+
+
 if not is_momod(msg) then
-    
-    
+
+
 chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-    local msgads = 'You can bad words!'
+    local msgtag = 'You cant link anything here '
    local receiver = msg.to.id
-    send_large_msg('chat#id'..receiver, msg.."\n", ok_cb, false)
-	
+    send_large_msg('chat#id'..receiver, msgads.."\n", ok_cb, false)
+
       end
    end
 end
-    
+
 return {patterns = {
 "کونی",
 "کیری",
@@ -66,7 +63,6 @@ return {patterns = {
 "بسیک",
 "سیکتر",
 "سیک",
-"خوارکسته",
-"خوارکسده",
-},
- run = run}
+"خوارکسته"
+}, run = run}
+
