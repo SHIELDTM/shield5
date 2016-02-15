1f8608bf0520e7443ef865e89dc52f4112f16ce4
shieldTM <hafez.janjan@gmail.com>
2016-02-15 20:46:02 +0330

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -1,25 +1,30 @@
-local function run(msg)
-
+local function run(msg, matches)
+    if is_owner(msg) then
+        return
+    end
     local data = load_data(_config.moderation.data)
-
-     if data[tostring(msg.to.id)]['settings']['lock_link'] == 'yes' then
-
-
-if not is_momod(msg) then
-
-
-chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-    local msgtag = 'You cant link anything here '
-   local receiver = msg.to.id
-    send_large_msg('chat#id'..receiver, msgads.."\n", ok_cb, false)
-
-      end
-   end
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_ads'] then
+                lock_ads = data[tostring(msg.to.id)]['settings']['lock_ads']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_ads == "yes" then
+        send_large_msg(chat, 'شما تبلیغ کردید و این در گروه ممنوع بود\nشما اخراج می شوید')
+        chat_del_user(chat, user, ok_cb, true)
+    end
 end
-
+ 
 return {patterns = {
-"https://(.*)",
-"http://(.*)",
-"www.(.*)",
-"telegram.me/(.*)"
-}, run = run}
+"[Hh]ttps://(.*)",
+"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/(.*)",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/(.*)/",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/(.*",
+"[Hh][Tt][Tt][Pp]://(.*)",
+"[Ww][Ww][Ww]:(.*)",
+  },
+  run = run
+}
