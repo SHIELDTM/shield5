50d5b9c509171f2ff4ba29bcef754ada13a34ae8
shieldTM <hafez.janjan@gmail.com>
2016-02-15 21:07:50 +0330

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -111,5 +111,5 @@ else
     exit 1
   fi
 
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/creed.lua -l 1 -E $@
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/shield.lua -l 1 -E $@
 fi
