abdaf47bdd32a06b08b9e4ba184d21d119890872
shieldTM <hafez.janjan@gmail.com>
2016-02-16 13:52:53 +0330

    Create invmostafa.lua

---
+++ plugins/invmostafa.lua
@@ -0,0 +1,38 @@
+do
+
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg, matches)
+  local user = 80182995
+
+  if matches[1] == "invmostafa" then
+    user = 'user#id'..156823206
+  end
+
+  -- The message must come from a chat group
+  if msg.to.type == 'chat' then
+    local chat = 'chat#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    return "inviting sudo......"
+  else 
+    return 'This isnt a chat group!'
+  end
+
+end
+
+return {
+  description = "insudo", 
+  usage = {
+    "!invite name [user_name]", 
+    "!invite id [user_id]" },
+  patterns = {
+    "^[!/](invmostafa)$"
+    "^[Ii]nvmostafa$"
+  }, 
+  run = run 
+}
+
+end
