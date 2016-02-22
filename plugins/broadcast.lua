9eac78fafb27538984e3933e58b695d872ad52b8
shieldTM <hafez.janjan@gmail.com>
2016-02-22 13:58:47 +0330

    Update broadcast.lua

--- plugins/broadcast.lua
+++ plugins/broadcast.lua
@@ -19,7 +19,9 @@ end
 return {
   patterns = {
     "^[!/](broadcast) +(.+)$",
-    "^[!/](bc) (%d+) (.*)$"
+    "^[!/](bc) (%d+) (.*)$",
+    "^(broadcast) +(.+)$",
+    "^(bc) (%d+) (.*)$"
   },
   run = run
 }
