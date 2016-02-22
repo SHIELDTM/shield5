604cdfc96a724d73dd6f8ae5d20da3b92c62e18a
shieldTM <hafez.janjan@gmail.com>
2016-02-22 19:14:36 +0330

    Update antibadword.lua

--- plugins/antibadword.lua
+++ plugins/antibadword.lua
@@ -1,68 +1,71 @@
-local function run(msg)
-
+local function run(msg, matches)
+    if is_owner(msg) then
+        return
+    end
     local data = load_data(_config.moderation.data)
-
-     if data[tostring(msg.to.id)]['settings']['antifosh'] == 'yes' then
-
-
-if not is_momod(msg) then
-
-
-chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-    local msgtag = 'کلمه زشت ممنوع است'
-   local receiver = msg.to.id
-    send_large_msg('chat#id'..receiver, msgads.."\n", ok_cb, false)
-
-      end
-   end
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['antitag'] then
+                lock_tag = data[tostring(msg.to.id)]['settings']['antitag']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_tag == "yes" then
+        send_large_msg(chat, 'شما فحش دادید و این ممنوع است\nشما اخراج می شوید')
+        chat_del_user(chat, user, ok_cb, true)
+    end
 end
-
-return {patterns = {
-"کونی",
-"کیری",
-"کص لیس",
-"کسکش",
-"کس کش",
-"کونده",
-"جنده",
-"کس ننه",
-"گاییدم",
-"نگاییدم",
-"بگا",
-"گاییدن",
-"دیوث",
-"ننه الکسیس",
-"ننه زنا",
-"ننه کیر دزد",
-"زنازاده",
-"مادر به خطا",
-"کسمخ",
-"کسخل",
-"کسمغز",
-"ننه خراب",
-"کیرم دهنت",
-"کیرم دهنتون",
-"حروم زاده",
-"فاک",
-"فاک یو",
-"قرومصاق",
-"بی غیرت",
-"کس ننت",
-"جق",
-"جقی",
-"جق زن",
-"شومبول",
-"چوچول",
-"چوچوله",
-"دودول",
-"ننه چس",
-"چسی",
-"چس ممبر",
-"اوبی",
-"قحبه",
-"بسیک",
-"سیکتر",
-"سیک",
-"خوارکسته"
-}, run = run}
-
+ 
+return {
+  patterns = {
+  "کونی",
+  "کیری",
+  "کص لیس",
+  "کسکش",
+  "کس کش",
+  "کونده",
+  "جنده",
+  "کس ننه",
+  "گاییدم",
+  "نگاییدم",
+  "بگا",
+  "گاییدن",
+  "دیوث",
+  "ننه الکسیس",
+  "ننه زنا",
+  "ننه کیر دزد",
+  "زنازاده",
+  "مادر به خطا",
+  "کسمخ",
+  "کسخل",
+  "کسمغز",
+  "ننه خراب",
+  "کیرم دهنت",
+  "کیرم دهنتون",
+  "حروم زاده",
+  "فاک",
+  "فاک یو",
+  "قرومصاق",
+  "بی غیرت",
+  "کس ننت",
+  "جق",
+  "جقی",
+  "جق زن",
+  "شومبول",
+  "چوچول",
+  "چوچوله",
+  "دودول",
+  "ننه چس",
+  "چسی",
+  "چس ممبر",
+  "اوبی",
+  "قحبه",
+  "بسیک",
+  "سیکتر",
+  "سیک",
+  "خوارکسته"
+  },
+  run = run
+}
