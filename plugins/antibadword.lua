57dac6212681f5cf33e0ffed184b8194b5b75fc1
shieldTM <hafez.janjan@gmail.com>
2016-02-24 17:24:33 +0330

    Update antibadword.lua

--- plugins/antibadword.lua
+++ plugins/antibadword.lua
@@ -5,67 +5,63 @@ local function run(msg, matches)
     local data = load_data(_config.moderation.data)
     if data[tostring(msg.to.id)] then
         if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['antitag'] then
-                lock_tag = data[tostring(msg.to.id)]['settings']['antitag']
+            if data[tostring(msg.to.id)]['settings']['antifosh'] then
+                lock_fosh = data[tostring(msg.to.id)]['settings']['antifosh']
             end
         end
     end
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
-    if lock_tag == "yes" then
-        send_large_msg(chat, 'شما فحش دادید و این ممنوع است\nشما اخراج می شوید')
+    if lock_fosh == "yes" then
+        send_large_msg(chat, 'به دلیل فحاشی اخرج می شوید\n\n@shieldTM')
         chat_del_user(chat, user, ok_cb, true)
     end
 end
  
 return {
   patterns = {
-  "کونی",
-  "کیری",
-  "کص لیس",
-  "کسکش",
-  "کس کش",
-  "کونده",
-  "جنده",
-  "کس ننه",
-  "گاییدم",
-  "نگاییدم",
-  "بگا",
-  "گاییدن",
-  "دیوث",
-  "ننه الکسیس",
-  "ننه زنا",
-  "ننه کیر دزد",
-  "زنازاده",
-  "مادر به خطا",
-  "کسمخ",
-  "کسخل",
-  "کسمغز",
-  "ننه خراب",
-  "کیرم دهنت",
-  "کیرم دهنتون",
-  "حروم زاده",
-  "فاک",
-  "فاک یو",
-  "قرومصاق",
-  "بی غیرت",
-  "کس ننت",
-  "جق",
-  "جقی",
-  "جق زن",
-  "شومبول",
-  "چوچول",
-  "چوچوله",
-  "دودول",
-  "ننه چس",
-  "چسی",
-  "چس ممبر",
-  "اوبی",
-  "قحبه",
-  "بسیک",
-  "سیکتر",
-  "سیک",
-  "خوارکسته"
+    "کس(.*)",
+    "کون(.*)",
+    "کیر(.*)",
+    "ممه(.*)",
+    "سکس(.*)",
+    "سیکتیر(.*)",
+    "قهبه(.*)",
+    "بسیک(.*)",
+    "بیناموس(.*)",
+    "اوبی(.*)",
+    "کونی(.*)",
+    "بیشرف(.*)",
+    "کس ننه(.*)",
+    "ساک(.*)",
+    "کیری(.*)",
+    "خار کوسه(.*)",
+    "ننه(.*)",
+    "خواهرتو(.*)",
+    "سکسی(.*)",
+    "کسکش(.*)",
+    "سیک تیر(.*)",
+    "گاییدم(.*)",
+    "میگام(.*)",
+    "میگامت(.*)",
+    "بسیک(.*)",
+    "خواهرت(.*)",
+    "خارتو(.*)",
+    "کونت(.*)",
+    "کوست(.*)",
+    "شورت(.*)",
+    "سگ(.*)",
+    "کیری(.*)",
+    "دزد(.*)",
+    "ننت(.*)",
+    "ابمو(.*)",
+    "جق(.*)",
+    "kos(.*)",
+    "koni(.*)",
+    "sik(.*)",
+    "jende(.*)",
+    "obi(.*)",
+    "nane(.*)"
   },
   run = run
 }
