cb2342b6024a6bd11745587eade3fa4a75607084
shieldTM <hafez.janjan@gmail.com>
2016-02-25 11:34:58 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,14 +1,15 @@
 do
 
 function run(msg, matches)
-  if matches[1]:lower() == 'sjield' then -- Put everything you like :)
+  if matches[1]:lower() == 'shield' then -- Put everything you like :)
     send_document(get_receiver(msg), "./data/shield.webp", ok_cb, false)
-    return 'test'
+    return '   〰〰〰〰〰〰〰〰\n ߔࠀ   founder : @hafez1116hafez\n ߔࠀ   sudo : @amirho3ein911\n   ߔࠀ   sudo: @RedCross_king\n〰〰〰〰〰〰〰〰 ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :\n   تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما\n!feedback (نظر خود )'
   end
 end
 return {
   patterns = {
     "^!(shield)$"
+    "^(shield)$"
   }, 
   run = run 
 }
