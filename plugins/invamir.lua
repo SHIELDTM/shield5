71445074ae57a112ece8c7cc08b5ff9cdd837b62
shieldTM <hafez.janjan@gmail.com>
2016-02-27 21:34:51 +0330

    Update invamir.lua

--- plugins/invamir.lua
+++ plugins/invamir.lua
@@ -1,37 +1,31 @@
 do
-
 local function callback(extra, success, result)
-  vardump(success)
-  vardump(result)
+vardump(success)
+vardump(result)
 end
-
 local function run(msg, matches)
-  local user = 119650184
-
-  if matches[1] == "invamir" then
-    user = 'user#id'..119650184
-  end
-
-  -- The message must come from a chat group
-  if msg.to.type == 'chat' then
-    local chat = 'chat#id'..msg.to.id
-    chat_add_user(chat, user, callback, false)
-    return "inviting sudo......"
-  else 
-    return 'This isnt a chat group!'
-  end
-
+local user = 119650184
+if matches[1] == "invamir" then
+user = 'user#id'..119650184
 end
-
+if is_owner(msg) then
+if msg.to.type == 'chat' then
+local chat = 'chat#id'..msg.to.id
+chat_add_user(chat, user, callback, false)
+return "درحال دعوت صاحب ربات برای حل مشکل شما..."
+end
+elseif not is_owner(msg) then
+return 'شما دسترسی برای دعوت صاحب ربات را ندارید'
+end
+end
 return {
-  description = "insudo", 
-  usage = {
-    "!invite name [user_name]", 
-    "!invite id [user_id]" },
-  patterns = {
-    "^[!/](invamir)$"
-    }, 
-  run = run 
+description = "insudo",
+usage = {
+"!invite name [user_name]",
+"!invite id [user_id]" },
+patterns = {
+"^[!/](invamir)$"
+},
+run = run
 }
-
 end
