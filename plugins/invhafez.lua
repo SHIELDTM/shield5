be2937a9ae979828cbf34d6a9cc0fa18251f571e
shieldTM <hafez.janjan@gmail.com>
2016-02-27 21:32:13 +0330

    Update invhafez.lua

--- plugins/invhafez.lua
+++ plugins/invhafez.lua
@@ -1,37 +1,31 @@
 do
-
 local function callback(extra, success, result)
-  vardump(success)
-  vardump(result)
+vardump(success)
+vardump(result)
 end
-
 local function run(msg, matches)
-  local user = 80182995
-
-  if matches[1] == "invhafez" then
-    user = 'user#id'..80182995
-  end
-
-  -- The message must come from a chat group
-  if msg.to.type == 'chat' then
-    local chat = 'chat#id'..msg.to.id
-    chat_add_user(chat, user, callback, false)
-    return "inviting sudo......"
-  else 
-    return 'This isnt a chat group!'
-  end
-
+local user = 80182995
+if matches[1] == "invhafez" then
+user = 'user#id'..80182995
 end
-
+if is_owner(msg) then
+if msg.to.type == 'chat' then
+local chat = 'chat#id'..msg.to.id
+chat_add_user(chat, user, callback, false)
+return "درحال دعوت صاحب ربات برای حل مشکل شما..."
+end
+elseif not is_owner(msg) then
+return 'شما دسترسی برای دعوت صاحب ربات را ندارید'
+end
+end
 return {
-  description = "insudo", 
-  usage = {
-    "!invite name [user_name]", 
-    "!invite id [user_id]" },
-  patterns = {
-    "^[!/](invhafez)$"
-  }, 
-  run = run 
+description = "insudo",
+usage = {
+"!invite name [user_name]",
+"!invite id [user_id]" },
+patterns = {
+"^[!/](invhafez)$"
+},
+run = run
 }
-
 end
