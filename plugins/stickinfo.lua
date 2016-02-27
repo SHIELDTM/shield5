fb274e6ffb7ae068f92514967a0154f2a4a40b9d
shieldTM <hafez.janjan@gmail.com>
2016-02-27 21:28:09 +0330

    Create stickinfo.lua

---
+++ plugins/stickinfo.lua
@@ -0,0 +1,26 @@
+do
+
+local function run(msg, matches)
+  if matches[1] == 'info' then
+    if is_sudo(msg) then
+    send_document(get_receiver(msg), "./data/sudo.webp", ok_cb, false)
+    elseif is_admin(msg) then
+    send_document(get_receiver(msg), "./data/admin.webp", ok_cb, false)
+    elseif is_owner(msg) then
+    send_document(get_receiver(msg), "./data/owner.webp", ok_cb, false)
+    elseif is_momod(msg) then
+    send_document(get_receiver(msg), "./data/mod.webp", ok_cb, false)
+    else
+    send_document(get_receiver(msg), "./data/member.webp", ok_cb, false)
+    end
+  end
+end
+
+return {
+  patterns = {
+    "^[!/]([Ii]nfo)$",
+    "^([Ii]nfo)$"
+    },
+  run = run
+}
+end
