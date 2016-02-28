42783b86a84c9f7e369a33265b505b3a27662f37
shieldTM <hafez.janjan@gmail.com>
2016-02-28 13:37:39 +0330

    Update addplug.lua

--- plugins/addplug.lua
+++ plugins/addplug.lua
@@ -4,20 +4,22 @@ do
     file:write(text)
     file:flush()
     file:close()
-    return "Plugin saved."
+    return "فایل ارسالی در سرور ذخیره شد"
 end   
 function run(msg, matches)
-  if matches[1] == "addplug" and is_sudo(msg) then
+  if matches[1] == "addplugin" and is_sudo(msg) then
  
          local name = matches[2]
         local text = matches[3]
         return save_file(name, text)
-        
-  end
+        end
+        if not is_sudo(msg) then 
+		return "شما دسترسی ندارید"
+	end
 end
 return {
   patterns = {
-  "^[!/](addplug) ([^%s]+) (.+)$"
+  "^[/!](addplugin) ([^%s]+) (.+)$"
   },
   run = run
 }
