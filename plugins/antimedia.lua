0e0350ec1406c1758c45366c641151b35bfa51ae
shieldTM <hafez.janjan@gmail.com>
2016-03-12 19:43:20 +0330

    Create antimedia.lua

---
+++ plugins/antimedia.lua
@@ -0,0 +1,30 @@
+local function run(msg, matches)
+    if is_owner(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['antimedia'] then
+                antimedia = data[tostring(msg.to.id)]['settings']['antimedia']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if antimedia == "yes" then
+        send_large_msg(chat, 'media is not allowed here')
+        chat_del_user(chat, user, ok_cb, true)
+    end
+end
+ 
+return {
+  patterns = {
+"%[(photo)%]",
+"%[(document)%]",
+"%[(video)%]",
+"%[(audio)%]",
+"%[(unsupported)%]",
+  },
+  run = run
+}
